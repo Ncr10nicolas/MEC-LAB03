dc139e014f4839e2990c25cd8b8d377625da7c0a
Ncr10nicolas <123744675+Ncr10nicolas@users.noreply.github.com>
2023-03-10 09:31:58 -0500

    Add files via upload

---
+++ ANIMACION/src/animacion/Main.java
@@ -0,0 +1,12 @@
+
+package animacion;
+
+
+public class Main {
+      
+    public static void main(String[] args) {
+        // TODO code application logic here
+        form1 f = new form1();
+        f.show();
+    }
+}
---
+++ laboratorio03nicolas/src/Laboratorio03nicolas.java
@@ -0,0 +1,100 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+
+public class Laboratorio03nicolas extends JFrame implements ActionListener {
+
+    private Timer timer;
+    private JLabel tiempoLabel;
+    private int horas, minutos, segundos;
+    private int alarmaMinutos, alarmaSegundos;
+    private boolean alarmaActivada;
+
+    public Laboratorio03nicolas() {
+        super("Cronómetro");
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setResizable(false);
+        setSize(250, 100);
+        setLayout(new FlowLayout());
+
+        tiempoLabel = new JLabel("00:00:00");
+        tiempoLabel.setFont(new Font("Arial", Font.PLAIN, 24));
+        add(tiempoLabel);
+
+        JButton startButton = new JButton("Iniciar");
+        startButton.addActionListener(this);
+        add(startButton);
+
+        JButton stopButton = new JButton("Detener");
+        stopButton.addActionListener(this);
+        add(stopButton);
+
+        alarmaActivada = false;
+
+        setVisible(true);
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        String command = e.getActionCommand();
+
+        if (command.equals("Iniciar")) {
+            iniciarCronometro();
+        } else if (command.equals("Detener")) {
+            detenerCronometro();
+        }
+    }
+
+    private void iniciarCronometro() {
+        timer = new Timer(1000, (ActionEvent e) -> {
+            segundos++;
+            
+            if (segundos == 60) {
+                segundos = 0;
+                minutos++;
+            }
+            
+            if (minutos == 60) {
+                minutos = 0;
+                horas++;
+            }
+            
+            actualizarTiempoLabel();
+            
+            if (alarmaActivada && minutos == alarmaMinutos && segundos == alarmaSegundos) {
+                activarAlarma();
+            }
+        });
+        timer.start();
+    }
+
+    private void detenerCronometro() {
+        timer.stop();
+    }
+
+    private void actualizarTiempoLabel() {
+        String tiempo = String.format("%02d:%02d:%02d", horas, minutos, segundos);
+        tiempoLabel.setText(tiempo);
+    }
+
+    private void activarAlarma() {
+        Timer alarmaTimer = new Timer(10000, new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                JOptionPane.showMessageDialog(null, "¡Alarma!");
+            }
+        });
+        alarmaTimer.start();
+    }
+
+    public void setAlarma(int minutos, int segundos) {
+        alarmaMinutos = minutos;
+        alarmaSegundos = segundos;
+        alarmaActivada = true;
+    }
+
+    public static void main(String[] args) {
+        Laboratorio03nicolas cronometro = new Laboratorio03nicolas();
+        cronometro.setAlarma(2, 0);
+    }
+
+}
